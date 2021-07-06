63b49cd3a98b8583e13dc611cda247e2772d95fe
shim seok bo <shadowog@gmail.com>
2021-07-06 18:52:45 +0900

    4. Declarative UI

--- app/src/main/java/fail/toepic/android/study/compose/pathway/composepathway/MainActivity.kt
+++ app/src/main/java/fail/toepic/android/study/compose/pathway/composepathway/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import fail.toepic.android.study.compose.pathway.composepathway.ui.theme.ComposePathwayTheme
 
@@ -26,7 +27,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    /** Greeting 함수를 위해 다른 배경생을 지정하려면 내용을 포함하는 Surrface를 선언해야합니다.
+     * Surface 내부에 중첩 된 요소는 Surface의 배경색 위에 그려집니다.  */
+    Surface(color = Color.Yellow) {
+        Text(text = "Hello $name!")
+    }
+
 }
 
 /* 인자가 없는 컴포즈 함수는  @Preview 어노테이션으로 미리보기가 가능합니다.
