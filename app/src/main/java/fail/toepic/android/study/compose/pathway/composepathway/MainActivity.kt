7f5ec04626c4e1cb2810e96602fab58c847f441c
shim seok bo <shadowog@gmail.com>
2021-07-06 19:18:12 +0900

    7. Animating your list

--- app/src/main/java/fail/toepic/android/study/compose/pathway/composepathway/MainActivity.kt
+++ app/src/main/java/fail/toepic/android/study/compose/pathway/composepathway/MainActivity.kt
@@ -3,15 +3,16 @@ package fail.toepic.android.study.compose.pathway.composepathway
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,16 +45,23 @@ fun MyApp(content : @Composable ()-> Unit){
 
 @Composable
 fun Greeting(name: String) {
-    /* Greeting 함수를 위해 다른 배경생을 지정하려면 내용을 포함하는 Surface 를 선언해야합니다.
-     * Surface 내부에 중첩 된 요소는 Surface 의 배경색 위에 그려집니다.  */
-    Surface(color = Color.Yellow) {
-        /* Modifier-수정자는 어떻게 배치 되고, 표시되며, 부모레이아웃 안에서 어떻게 동작 하는지에 대하여 말해줍니다.
-        * 변수에 할당하고 재활용 될 수 있으며, 몇개의 수정자를 채이닝 하거나, 팩토리 확장함수 등을 이용하여 연속사용 할 수 있습니다? (맞나?_
-        * padding 은 요소 주변의 주어진 수치의 공간을 추가합니다.  */
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
-    }
 
+    var isSelected by remember { mutableStateOf(false) }
+    /* 이 특정 사용 사례에서는 animateColorAsStateColor 를 매개 변수로 사용하여 저장하고
+     이전에 설정된 색상에서 새 색상으로 애니메이션 전환을 표시하는 데 필요한 중간 색상을 자동으로 생성합니다 */
+    val backgroundColor by animateColorAsState(targetValue = if (isSelected) Color.Red else Color.Transparent)
+
+
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .padding(24.dp)
+            /* background 수정자를 이용하여 배경 변경을 추가할 수있습니다.  */
+            .background(color = backgroundColor)
+            .clickable(onClick = {isSelected = !isSelected})
+    )
 }
+
 
 /* 인자가 없는 컴포즈 함수는  @Preview 어노테이션으로 미리보기가 가능합니다.
 *  name 인자를 이용하여 프리뷰 이름을 지정할 수 있습니다. */
