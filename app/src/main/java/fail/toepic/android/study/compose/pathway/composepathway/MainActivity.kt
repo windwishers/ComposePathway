d4db9b5d6dba7495410c35daa266c6b8978f6a33
shim seok bo <shadowog@gmail.com>
2021-07-06 18:44:42 +0900

    initialize Compose PathWay

---
+++ app/src/main/java/fail/toepic/android/study/compose/pathway/composepathway/MainActivity.kt
@@ -0,0 +1,38 @@
+package fail.toepic.android.study.compose.pathway.composepathway
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import fail.toepic.android.study.compose.pathway.composepathway.ui.theme.ComposePathwayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposePathwayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposePathwayTheme {
+        Greeting("Android")
+    }
+}
